14d29be5070f3c4d39b4631b5964d31ebba7f79a
simon ferreira de carvalho <simoncarvalho10>
2025-01-20 10:13:04 -0300

    deploy 5

--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {cors: {}});
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 3002 });
+const wss = new WebSocket.Server({ port: process.env.PORT || 3002 });
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 wss.on('connection', (ws) => {
 
